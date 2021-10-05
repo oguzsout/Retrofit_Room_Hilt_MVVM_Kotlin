1b859136db906ac35cfc52bd81254b5f14fd5d92
Oguzhan Dogdu <oguzd494@gmail.com>
2021-10-05 17:07:57 +0300

    Added dependencies

---
+++ app/src/main/java/com/oguzdogdu/retrofit_room_hilt_mvvm_kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.oguzdogdu.retrofit_room_hilt_mvvm_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
